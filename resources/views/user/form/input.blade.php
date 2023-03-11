78f4cef8dfe9c23d41cc5f2dd124ba1e9d6a8cef
hat2203 <hantth2203018@fpt.edu.vn>
2023-03-11 22:04:28 +0700

    hacommit

---
+++ resources/views/user/form/input.blade.php
@@ -0,0 +1,9 @@
+
+    <label>{{$label}}</label>
+    <input type="{{$type}}" value="{{isset($value)?$value:old($key)}}" name="{{$key}}"
+           class="@error($key)is-invalid @enderror form-control"
+           @if($required) required @endif>
+    @error($key)
+    <p class="text-danger">{{$message}}</p>
+    @enderror
+
