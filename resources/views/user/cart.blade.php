507f2ba81fab7dad3c5aba18882159315d6c677f
UngSyMui <ungsymui@gmail.com>
2023-03-15 17:26:54 +0700

    migration

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -1,5 +1,5 @@
 @extends('user.layout')
-@section("title","Payment All")
+@section("title","Shop Cart")
 @section('center-content')
     <div class="db-2">
         <form>
@@ -20,18 +20,28 @@
                         </tr>
                         </thead>
                         <tbody>
+                        @forelse( $cart as $item)
                         <tr>
+                            <td>{{$item->id}}</td>
+                            <td>{{$item->TypeOfTicket->Flight->AirStrip->TakeofCity->name}}</td>
+                            <td>{{$item->TypeOfTicket->Flight->AirStrip->LandingCity->name}}</td>
+                            <td>{{$item->TypeOfTicket->Flight->takeoftime}} - {{$item->TypeOfTicket->Flight->landingtime}} </td>
+                            <td>{{$item->TypeOfTicket->name}}</td>
+                            <td>{{$item->price}}</td>
                             <td>1</td>
-                            <td>Ha Noi</td>
-                            <td>Ho Chi Minh</td>
-                            <td>11:00:00 - 13:00:00</td>
-                            <td>VIP</td>
-                            <td>300.000</td>
-                            <td>1</td>
-                            <td>300.000</td>
-                            </td>
+                            <td>{{$item->price}}</td>
+
                         </tr>
+                        @empty
+                            <tr>
+                                <td>
+                                    <p>No ticket</p>
+                                </td>
+                            </tr>
+                        @endforelse
+
                         </tbody>
+
                     </table>
                     <div class="col-md-6">
                         <h5>Discount</h5>
@@ -39,11 +49,22 @@
                         <p style="font-size: 12px">If you have a discount code please enter the code</p>
                     </div>
                 </div>
+                @if($grand_total > 0)
+
                 <div class="row">
                     <div class="col-md-4" style="float: right;margin-right: 30px">
-                        <span class="db-pay-amount" style="color: rgba(255,0,0,0.89)">Total: 300.000</span>
+
+                        <span class="db-pay-amount" style="color: rgba(255,0,0,0.89)">{{$grand_total}}</span>
+
                     </div>
+                    @if($can_checkout)
+                        <a href="{{url("")}}" class="primary-btn">PROCEED TO CHECKOUT</a>
+                    @else
+                        <a href="javascript:void(0);" style="background-color: gray;" class="primary-btn disabled">PROCEED TO CHECKOUT</a>
+                    @endif
+
                 </div>
+                @endif
                 <div class="row">
                     <div class="col-md-2" style="float: right;margin-right: 30px">
                         <div class="hot-page2-alp-ri-p3 tour-alp-ri-p3">
