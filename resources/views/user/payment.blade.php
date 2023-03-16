c41850cb46a87028c9e9d2d704eafd53b4714f15
UngSyMui <ungsymui@gmail.com>
2023-03-16 20:48:41 +0700

    migration

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -7,41 +7,40 @@
             <div class="db-2-main-com db2-form-pay db2-form-com">
                 <form class="col s12"  method="post" action="{{url("/checkout")}}">
                     @csrf
+                    @for($i=0;$i<$qty;$i++)
                     <div class="row">
                         <div class="input-field col s12">
                             <h5>Name</h5>
-                            <input type="text" class="validate">
+                            <input name="name{{$i}}" type="text" class="validate" required>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m6">
                             <h5>Gender</h5>
-                            <select>
-                                <option value="0">Male</option>
-                                <option value="1">Female</option>
-                            </select>
+                            <input name="gender{{$i}}" type="text" class="validate" required>
                         </div>
                         <div class="input-field col s12 m6">
                             <h5>Date of birth</h5>
-                            <input type="date" >
+                            <input name="birthday{{$i}}" type="date" required>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m6">
                             <h5>Email</h5>
-                            <input type="email" class="validate">
+                            <input name="emmail{{$i}}" type="email" class="validate" required>
                         </div>
                         <div class="input-field col s12 m6">
                             <h5>Phone Number</h5>
-                            <input type="text" class="validate">
+                            <input name="phone{{$i}}" type="text" class="validate">
                         </div>
                     </div>
                     <div class="row ">
                         <div class="input-field col s12 ">
                             <h5>Address</h5>
-                            <input type="text" class="validate">
+                            <input name="address{{$i}}" type="text" class="validate">
                         </div>
                     </div>
+                    @endfor
                     <div class="row">
                         <span class="db-pay-amount">Total of ticket: {{$qty}}</span>
                     </div>
