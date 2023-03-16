c84b5b9fa125cbcbf2a2f4aa57ebb2d36ca3ce64
hat2203 <hantth2203018@fpt.edu.vn>
2023-03-16 16:30:01 +0700

    frond-end

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -1,8 +1,7 @@
 @extends('user.layout')
-@section("title","Shop Cart")
+@section("title","Cart")
 @section('center-content')
     <div class="db-2">
-        <form>
             <div class="db-2-com db-2-main">
                 <h4>Your ticket</h4>
                 <div class="db-2-main-com db-2-main-com-table db2-form-pay db2-form-com">
@@ -22,20 +21,20 @@
                         </thead>
                         <tbody>
                         @forelse( $cart as $item)
-                        <tr>
-                            <td>{{$item->id}}</td>
-                            <td>{{$item->TypeOfTicket->Flight->AirStrip->TakeofCity->name}}</td>
-                            <td>{{$item->TypeOfTicket->Flight->AirStrip->LandingCity->name}}</td>
-                            <td>{{$item->TypeOfTicket->Flight->takeoftime}} - {{$item->TypeOfTicket->Flight->landingtime}} </td>
-                            <td>{{$item->TypeOfTicket->name}}</td>
-                            <td>{{$item->price}}</td>
-                            <td>1</td>
-                            <td>{{$item->price}}</td>
-                            <td class="shoping__cart__item__close">
-                                <a href="{{url("/remove-cart",["ticket"=>$item->id])}}">Delete</a>
-                            </td>
+                            <tr>
+                                <td>{{$item->id}}</td>
+                                <td>{{$item->TypeOfTicket->Flight->AirStrip->TakeofCity->name}}</td>
+                                <td>{{$item->TypeOfTicket->Flight->AirStrip->LandingCity->name}}</td>
+                                <td>{{$item->TypeOfTicket->Flight->takeoftime}} - {{$item->TypeOfTicket->Flight->landingtime}} </td>
+                                <td>{{$item->TypeOfTicket->name}}</td>
+                                <td>{{$item->price}}</td>
+                                <td>1</td>
+                                <td>{{$item->price}}</td>
+                                <td class="shoping__cart__item__close">
+                                    <a href="{{url("/remove-cart",["ticket"=>$item->id])}}">Delete</a>
+                                </td>
 
-                        </tr>
+                            </tr>
                         @empty
                             <tr>
                                 <td>
@@ -55,28 +54,28 @@
                 </div>
                 @if($grand_total > 0)
 
-                <div class="row">
-                    <div class="col-md-4" style="float: right;margin-right: 30px">
+                    <div class="row">
+                        <div class="col-md-4" style="float: right;margin-right: 30px">
 
-                        <span class="db-pay-amount" style="color: rgba(255,0,0,0.89)">{{$grand_total}}</span>
+                            <span class="db-pay-amount" style="color: rgba(255,0,0,0.89)">{{$grand_total}}</span>
 
-                    </div>
-                    @if($can_checkout)
-                        <a href="{{url("")}}" class="primary-btn">PROCEED TO CHECKOUT</a>
-                    @else
-                        <a href="javascript:void(0);" style="background-color: gray;" class="primary-btn disabled">PROCEED TO CHECKOUT</a>
-                    @endif
-
-                </div>
-                @endif
-                <div class="row">
-                    <div class="col-md-2" style="float: right;margin-right: 30px">
-                        <div class="hot-page2-alp-ri-p3 tour-alp-ri-p3">
-                            <a href="{{url("/payment")}}" style="width: 100px" class="btn btn-warning">Done</a>
                         </div>
+                        @if($can_checkout)
+{{--                            <a href="{{url("")}}" class="primary-btn">PROCEED TO CHECKOUT</a>--}}
+                            <div class="row">
+                                <div class="col-md-2" style="float: right;margin-right: 30px">
+                                    <div class="hot-page2-alp-ri-p3 tour-alp-ri-p3">
+                                        <a href="{{url("/checkout")}}" style="width: 100px" class="btn btn-warning">Done</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @else
+                            <a href="javascript:void(0);" style="background-color: gray;" class="primary-btn disabled">PROCEED TO CHECKOUT</a>
+                        @endif
+
                     </div>
-                </div>
+                @endif
+
             </div>
-        </form>
     </div>
 @endsection
