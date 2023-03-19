ae83bad12f184d3cc8b05e3cc386a19042af0f28
UngSyMui <ungsymui@gmail.com>
2023-03-19 13:16:02 +0700

    migration

--- resources/views/admin/html/notification.blade.php
+++ resources/views/admin/html/notification.blade.php
@@ -10,7 +10,7 @@
         <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <h5><i class="icon fas fa-check"></i> Alert!</h5>
-           {{session("success")}}
+            {{session("success")}}
 
         </div>
     @endif
