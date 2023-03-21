1734703e389701833da89c653514ad057380890b
UngSyMui <ungsymui@gmail.com>
2023-03-21 19:28:58 +0700

    migration

--- resources/views/admin/html/notification.blade.php
+++ resources/views/admin/html/notification.blade.php
@@ -2,14 +2,14 @@
     @if(session()->has("error"))
         <div class="alert alert-warning alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h5><i class="icon fas fa-exclamation-triangle"></i> Alert!</h5>
+{{--            <h5><i class="icon fas fa-exclamation-triangle"></i> Alert!</h5>--}}
             {{session("error")}}
         </div>
     @endif
     @if(session()->has("success"))
         <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h5><i class="icon fas fa-check"></i> Alert!</h5>
+{{--            <h5><i class="icon fas fa-check"></i> Alert!</h5>--}}
             {{session("success")}}
 
         </div>
